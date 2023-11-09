f44337c659effb90b2e24156e071a7c088760636
Esteban Sepulveda <tebansd@icloud.com>
2023-11-08 19:59:21 -0800

    implemented bare bones weather home screen content

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 
-export default function App() {
-  const message = 'Hello'
+const App = () => {
   return (
-    <View style={{margin: 20, height: 100, width: 100, backgroundColor: 'orange'}}>
-      <Text>{message}</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+    <View style={styles.container}>
+      <Text style={styles.temp}>6</Text>
+      <Text style={styles.feels}>Feels like 5</Text>
+      <View style={styles.highLowWrapper}>
+        <Text style={styles.highLow}>High: 8</Text>
+        <Text style={styles.highLow}>Low: 6</Text>
+      </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style ={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'pink'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    fontSize: 30,
+    color: 'Black'
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
+})
+export default App
