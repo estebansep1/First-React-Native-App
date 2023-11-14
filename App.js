82d0c76546b15bb076f336d4d083823891ea8777
Esteban Sepulveda <tebansd@icloud.com>
2023-11-13 17:14:02 -0800

    added openweather API

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
-/* eslint-disable react/jsx-no-undef */
-import React, { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-catch-shadow */
+/* eslint-disable no-shadow */
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/Components/Tabs'
+import * as Location from 'expo-location'
+import { WEATHER_API_KEY } from '@env'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const data = await res.json()
+    setWeather(data)
+    } catch (error) {
+        setError('Could not fetch weather')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location denied')
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
+    })()
+  }, [lat, lon])
+
+  if (weather) {
+    console.log(weather)
+  }
+
   if (loading) {
     return (
       <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'}/>
+        <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Tabs />
@@ -23,7 +63,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    flex: '1'
+    flex: 1
   }
 })
 
