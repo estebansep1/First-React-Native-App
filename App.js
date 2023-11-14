47c11d2db022ba9bebe1b26718b62f2c0b4eabfe
Esteban Sepulveda <tebansd@icloud.com>
2023-11-14 12:43:51 -0800

    refactored useGetWeather part of code

--- App.js
+++ App.js
@@ -1,49 +1,15 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-catch-shadow */
-/* eslint-disable no-shadow */
-/* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react'
 import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/Components/Tabs'
 import * as Location from 'expo-location'
 import { WEATHER_API_KEY } from '@env'
+import useGetWeather from './src/Hooks/useGetWeather'
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
-
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-    const data = await res.json()
-    setWeather(data)
-    } catch (error) {
-        setError('Could not fetch weather')
-      } finally {
-        setLoading(false)
-      }
-    }
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        setError('permission to access location denied')
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if (weather) {
-    console.log(weather)
-  }
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
 
   if (loading) {
     return (
