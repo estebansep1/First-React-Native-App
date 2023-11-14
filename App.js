25bcf74e844cf79687bfd6d9d1ec9494e8579c61
Esteban Sepulveda <tebansd@icloud.com>
2023-11-13 16:19:07 -0800

    added demo useState code for practice, implemented ActivityIndicator

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react'
+/* eslint-disable react/jsx-no-undef */
+import React, { useState } from 'react'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/Components/Tabs'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'}/>
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: '1'
+  }
+})
 
 export default App
